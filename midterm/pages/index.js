b05c4e8029932d0682be7ae588a847ee73f2cd8c
Kimsumin123 <tntn211@naver.com>
2023-10-18 13:19:55 +0900

    1번째 커밋

--- midterm/pages/index.js
+++ midterm/pages/index.js
@@ -1,3 +1,18 @@
-export default function main() {
-  return "202130403 김수민";
-}
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+const Test = () => {
+  const [routes, setRoutes] = useState("");
+
+  const router = useRouter();
+  const account = router.query[0];
+  useEffect(() => {
+    if (account && typeof account === "object" && account.length > 0) {
+      setRoutes(account);
+    }
+  }, [account]);
+  
+  return <div>202130403 김수민{routes}</div>;
+};
+
+export default Test;
